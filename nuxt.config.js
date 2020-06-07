5cd4e2b6a9dbbe6c85832c75d221447a4c43db34
reemhosny <reemhmahdy@gmail.com>
2020-06-08 00:47:12 +0200

    remove icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Comperha',
+    title: 'Compareha',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
